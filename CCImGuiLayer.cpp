c1609a43d1cfb642a818f7b1362797906183b436
TomoyukiMizuma <nishino123tomo@gmail.com>
2019-09-18 15:04:28 +0900

    first commit

---
+++ CCImGuiLayer.cpp
@@ -0,0 +1,59 @@
+#include "CCImGuiLayer.h"
+#include "imgui/imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "CCIMGUI.h"
+
+ImGuiLayer* ImGuiLayer::create()
+{
+    ImGuiLayer* pRet = new (std::nothrow) ImGuiLayer();
+    if (pRet && pRet->init())
+    {
+        pRet->autorelease();
+        return pRet;
+    }
+    CC_SAFE_DELETE(pRet);
+    return nullptr;
+}
+
+bool ImGuiLayer::init()
+{
+    setGLProgram(cocos2d::GLProgramCache::getInstance()->getGLProgram(cocos2d::GLProgram::SHADER_NAME_POSITION_COLOR));
+
+    // note: when at the first click to focus the window, this will not take effect
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto listener = cocos2d::EventListenerTouchOneByOne::create();
+    listener->setSwallowTouches(true);
+    listener->onTouchBegan = [](cocos2d::Touch* touch, cocos2d::Event*) -> bool {
+        return ImGui::IsAnyWindowHovered();
+    };
+    getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+#endif
+    return true;
+}
+
+void ImGuiLayer::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4 &parentTransform, uint32_t parentFlags)
+{
+    Node::visit(renderer, parentTransform, parentFlags);
+    _command.init(_globalZOrder);
+    _command.func = CC_CALLBACK_0(ImGuiLayer::onDraw, this);
+    cocos2d::Director::getInstance()->getRenderer()->addCommand(&_command);
+}
+
+void ImGuiLayer::onDraw()
+{
+    getGLProgram()->use();
+
+    // create frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // draw all gui
+    CCIMGUI::getInstance()->update();
+
+    // render
+    glUseProgram(0);
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
---
+++ Sample_imgui_Scene.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cocos2d.h>
+class Sample_imgui_Scene : public cocos2d::Scene
+{
+public:
+    static Sample_imgui_Scene* create();
+    virtual void onEnter() override;
+};
